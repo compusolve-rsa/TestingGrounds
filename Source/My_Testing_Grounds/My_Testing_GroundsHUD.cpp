bde8838c25c644fed84a42c8c48f88eba3e5a8f6
pvlunegov <pvlunegov@mail.ru>
2019-01-02 16:06:27 +0500

    TG 18. Refactoring file folder content system. Now folders organized much better and nice may. Deleted trash content.

--- Source/My_Testing_Grounds/My_Testing_GroundsHUD.cpp
+++ Source/My_Testing_Grounds/My_Testing_GroundsHUD.cpp
@@ -10,7 +10,7 @@
 AMy_Testing_GroundsHUD::AMy_Testing_GroundsHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Player/Textures/FirstPersonCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
